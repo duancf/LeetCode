b14e2c3b0028528135e88950940d45d9df0cbe4e
duancf <duancfmail@163.com>
2023-10-08 10:34:03 +0800

    init

---
+++ code/src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * @author duancf
+ * @version 1.0
+ * @date created in 2023年10月08日 10:29
+ * @since 1.0
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
